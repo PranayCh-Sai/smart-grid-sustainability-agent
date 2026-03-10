ce95d409e85e0887fe87908becb2044b002be619
PranayCh-Sai <pranaysai4321@gmail.com>
2026-03-10 21:44:10 +0000

    Delete Notebooks/00_setup_database.sql

--- Notebooks/00_setup_database.sql
+++
@@ -1,9 +0,0 @@
--- This script sets up the 'Environment' for our project.
--- We use a Catalog to keep everything organized in one place.
-
-CREATE CATALOG IF NOT EXISTS smart_grid;
-
--- We create three schemas (folders) for our Medallion Architecture.
-CREATE SCHEMA IF NOT EXISTS smart_grid.bronze; -- Raw API Data
-CREATE SCHEMA IF NOT EXISTS smart_grid.silver; -- Cleaned & Enriched Data
-CREATE SCHEMA IF NOT EXISTS smart_grid.gold;   -- Final Business Reports
